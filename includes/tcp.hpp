af68e554b0d0655f656e86d08676863d50bcbe79
ansh1406 <anshswaroop14@gmail.com>
2025-12-18 17:51:52 +0000

    First draft of TCP socket wrapper

---
+++ includes/tcp.hpp
@@ -0,0 +1,155 @@
+#ifndef TCP_HPP
+#define TCP_HPP
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <cstdint>
+#include <stdexcept>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+
+namespace tcp
+{
+    typedef int SocketHandle;
+    typedef uint16_t Port;
+
+    namespace constants
+    {
+        const SocketHandle INVALID_SOCKET = -1;
+        const int SOCKET_ERROR = -1;
+        const Port DEFAULT_PORT = 51234;
+        const in_addr_t DEFAULT_ADDRESS = INADDR_ANY;
+        const int BACKLOG = 128;
+        const ssize_t MAX_BUFFER_SIZE = 4096;
+        const int OPTION_TRUE = 1;
+    }
+
+    namespace exceptions
+    {
+        class CanNotCreateSocket : public std::runtime_error
+        {
+        public:
+            explicit CanNotCreateSocket(const std::string error) : std::runtime_error(error) {}
+        };
+        class CanNotBindSocket : public std::runtime_error
+        {
+        public:
+            explicit CanNotBindSocket(const std::string error) : std::runtime_error(error) {}
+        };
+        class CanNotSetSocketOptions : public std::runtime_error
+        {
+        public:
+            explicit CanNotSetSocketOptions(const std::string error) : std::runtime_error(error) {}
+        };
+        class SocketNotCreated : public std::runtime_error
+        {
+        public:
+            explicit SocketNotCreated(const std::string error) : std::runtime_error(error) {}
+        };
+        class CanNotListenOnSocket : public std::runtime_error
+        {
+        public:
+            explicit CanNotListenOnSocket(const std::string error) : std::runtime_error(error) {}
+        };
+        class CanNotAcceptConnection : public std::runtime_error
+        {
+        public:
+            explicit CanNotAcceptConnection(const std::string error) : std::runtime_error(error) {}
+        };
+        class CanNotSendData : public std::runtime_error
+        {
+        public:
+            explicit CanNotSendData(const std::string error) : std::runtime_error(error) {}
+        };
+        class CanNotReceiveData : public std::runtime_error
+        {
+        public:
+            explicit CanNotReceiveData(const std::string error) : std::runtime_error(error) {}
+        };
+    }
+
+    class SocketFD
+    {
+    private:
+        SocketHandle fd_;
+
+    public:
+        explicit SocketFD(SocketHandle handle = constants::INVALID_SOCKET) : fd_(handle) {}
+
+        SocketFD(const SocketFD &) = delete;
+        SocketFD &operator=(const SocketFD &) = delete;
+
+        SocketFD(SocketFD &&other) noexcept : fd_(other.fd_)
+        {
+            other.fd_ = constants::INVALID_SOCKET;
+        }
+        SocketFD &operator=(SocketFD &&other) noexcept
+        {
+            if (this != &other)
+            {
+                close_fd();
+                fd_ = other.fd_;
+                other.fd_ = constants::INVALID_SOCKET;
+            }
+            return *this;
+        }
+
+        ~SocketFD()
+        {
+            close_fd();
+        }
+
+        SocketHandle fd() const { return fd_; }
+        explicit operator bool() const { return fd_ != constants::INVALID_SOCKET; }
+
+    private:
+        void close_fd()
+        {
+            if (fd_ != constants::INVALID_SOCKET)
+            {
+                ::close(fd_);
+                fd_ = constants::INVALID_SOCKET;
+            }
+        }
+    };
+
+    class ConnectionSocket
+    {
+    private:
+        SocketFD socket_fd;
+
+    public:
+        explicit ConnectionSocket(const SocketHandle handle) : socket_fd(handle) {}
+        SocketHandle fd() const
+        {
+            return socket_fd.fd();
+        }
+        void send_data(const std::vector<int8_t> &data);
+        std::vector<int8_t> receive_data(const size_t max_size = constants::MAX_BUFFER_SIZE);
+    };
+    class ListeningSocket
+    {
+    private:
+        sockaddr_in address;
+        sockaddr_in create_address(const in_addr_t ip, const Port port);
+        SocketFD create_socket();
+        void bind_socket();
+        void start_listening();
+        SocketFD socket_fd;
+
+    public:
+        explicit ListeningSocket(const in_addr_t ip = constants::DEFAULT_ADDRESS, const Port port = constants::DEFAULT_PORT);
+        SocketHandle fd() const
+        {
+            return socket_fd.fd();
+        }
+        ConnectionSocket accept_connection();
+    };
+
+}
+#endif
---
+++ src/tcp.cpp
@@ -0,0 +1,124 @@
+#include "includes/tcp.hpp"
+tcp::ListeningSocket::ListeningSocket(const in_addr_t ip, const tcp::Port port)
+{
+    try
+    {
+        address = create_address(ip, port);
+        socket_fd = create_socket();
+        bind_socket();
+        start_listening();
+    }
+    catch (const tcp::exceptions::CanNotCreateSocket &e)
+    {
+        std::cerr << e.what() << std::endl;
+        throw tcp::exceptions::SocketNotCreated{"Cannot create socket."};
+    }
+    catch (const tcp::exceptions::CanNotSetSocketOptions &e)
+    {
+        std::cerr << e.what() << std::endl;
+        throw tcp::exceptions::SocketNotCreated{"Cannot set socket options."};
+    }
+    catch (const tcp::exceptions::CanNotBindSocket &e)
+    {
+        std::cerr << e.what() << std::endl;
+        throw tcp::exceptions::SocketNotCreated{"Cannot bind socket."};
+    }
+    catch (const tcp::exceptions::CanNotListenOnSocket &e)
+    {
+        std::cerr << e.what() << std::endl;
+        throw tcp::exceptions::SocketNotCreated{"Cannot listen on socket."};
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown error occurred during socket creation" << std::endl;
+        throw tcp::exceptions::SocketNotCreated{"Unknown error while setting up the socket."};
+    }
+}
+
+tcp::SocketFD tcp::ListeningSocket::create_socket()
+{
+    SocketFD sock(socket(AF_INET, SOCK_STREAM, 0));
+    if (sock.fd() < 0)
+    {
+        int err = errno;
+        throw tcp::exceptions::CanNotCreateSocket{std::string(strerror(err))};
+    }
+    int optionValue = tcp::constants::OPTION_TRUE;
+    if (setsockopt(sock.fd(), SOL_SOCKET, SO_REUSEADDR, &optionValue, sizeof(int)) < 0)
+    {
+        int err = errno;
+        throw tcp::exceptions::CanNotSetSocketOptions{std::string(strerror(err))};
+    }
+    return sock;
+}
+
+sockaddr_in tcp::ListeningSocket::create_address(const in_addr_t ip, const tcp::Port port)
+{
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = ip;
+    addr.sin_port = htons(port);
+    return addr;
+}
+
+void tcp::ListeningSocket::bind_socket()
+{
+    if (bind(socket_fd.fd(), reinterpret_cast<sockaddr *>(&address), sizeof(address)) < 0)
+    {
+        int err = errno;
+        throw tcp::exceptions::CanNotBindSocket{std::string(strerror(err))};
+    }
+}
+
+void tcp::ListeningSocket::start_listening()
+{
+    if (listen(socket_fd.fd(), tcp::constants::BACKLOG) < 0)
+    {
+        int err = errno;
+        throw tcp::exceptions::CanNotListenOnSocket{std::string(strerror(err))};
+    }
+}
+
+tcp::ConnectionSocket tcp::ListeningSocket::accept_connection()
+{
+    sockaddr_in client_addr{};
+    socklen_t client_len = sizeof(client_addr);
+    ConnectionSocket client_socket(accept(socket_fd.fd(), reinterpret_cast<struct sockaddr *>(&client_addr), &client_len));
+    if (client_socket.fd() < 0)
+    {
+        int err = errno;
+        throw tcp::exceptions::CanNotAcceptConnection{std::string(strerror(err))};
+    }
+    return client_socket;
+}
+
+void tcp::ConnectionSocket::send_data(const std::vector<int8_t> &data)
+{
+    ssize_t total_sent = 0;
+    ssize_t data_length = data.size();
+    const int8_t *data_ptr = data.data();
+
+    while (total_sent < data_length)
+    {
+        ssize_t bytes_sent = send(socket_fd.fd(), data_ptr + total_sent, data_length - total_sent, 0);
+        if (bytes_sent < 0)
+        {
+            int err = errno;
+            throw tcp::exceptions::CanNotSendData{std::string(strerror(err))};
+        }
+        total_sent += bytes_sent;
+    }
+}
+
+std::vector<int8_t> tcp::ConnectionSocket::receive_data(const size_t max_size)
+{
+    std::vector<int8_t> buffer(max_size);
+    ssize_t bytes_received = recv(socket_fd.fd(), buffer.data(), max_size, 0);
+    if (bytes_received < 0)
+    {
+        int err = errno;
+        throw tcp::exceptions::CanNotReceiveData{std::string(strerror(err))};
+    }
+    /// @todo : Watch for EINTR and EAGAIN
+    return buffer;
+}
