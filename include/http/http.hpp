541dcd6a6b53456d7fadabfa447a23189b6d0a0b
ansh1406 <anshswaroop14@gmail.com>
2026-02-08 17:23:16 +0530

    Documentation

--- include/http/http.hpp
+++ include/http/http.hpp
@@ -1,3 +1,15 @@
+/*
+
+Http Server is a simple and efficient HTTP server library implemented in C++.
+It provides a straightforward interface for handling HTTP requests and responses, making it easy to create web servers and APIs.
+The library supports HTTP/1.1 and includes features such as request parsing, response generation, and inbuilt connection management.
+With its modular design, developers can easily extend its functionality to suit their specific needs.
+
+*/
+
+///@file http.hpp
+///@brief This file includes all the necessary headers for using the HTTP server library. It provides a convenient way to include all the components of the library with a single include statement.
+
 #ifndef HTTP_HPP
 #define HTTP_HPP
 
@@ -9,11 +21,16 @@
 #include <string>
 #include <stdexcept>
 
+
+/// @brief Namespace for the HTTP server library. All the classes, functions, and constants related to the HTTP server are defined within this namespace.
 namespace http
 {
+    /// @brief Type alias for the request handler function. It is a std::function that takes a const reference to an HttpRequest and a non-const reference to an HttpResponse, and returns void. This function will be called for each incoming HTTP request, allowing the user to process the request and generate an appropriate response.
     using RequestHandler = std::function<void(const http::HttpRequest &, http::HttpResponse &)>;
+
     namespace exceptions
     {
+        /// @brief Exception class for errors that occur when the server cannot be created. It inherits from std::runtime_error and provides a constructor that takes an optional message to provide more details about the error.
         class CanNotCreateServer : public std::runtime_error
         {
         public:
@@ -22,13 +39,18 @@ namespace http
         };
     }
 
+    /// @brief Configuration structure for the HTTP server. It contains various parameters that can be set to configure the behavior of the server, such as the port to listen on, maximum pending connections, maximum concurrent connections, timeout for inactive connections, and whether to enable external logging.
     struct HttpServerConfig
     {
+        /// @brief The port number on which the HTTP server will listen for incoming connections. It is an unsigned short integer, and the default value is typically 80 for HTTP.
         unsigned short port;
+        /// @brief The maximum number of pending connections that the server can have in its queue. This parameter controls how many incoming connections can be waiting to be accepted before the server starts rejecting new connections. It is an unsigned integer.
         unsigned int max_pending_connections;
+        /// @brief The maximum number of concurrent connections that the server can handle at any given time. It is an unsigned integer. If the number of active connections exceeds this limit, the server may start rejecting new connections until some of the existing connections are closed.
         unsigned int max_concurrent_connections;
         /// @brief The timeout duration in seconds for inactive connections. If a connection remains idle (i.e., no data is sent or received) for longer than this duration, the server may close the connection to free up resources. It is a time_t value.
         time_t inactive_connection_timeout_in_seconds;
+        /// @brief A boolean flag indicating whether to enable external logging. If set to true, the server will log information about incoming requests, responses, and other events to an external logging system. If set to false, the server will log to stdout and stderr if external logging is disabled. The default value is false.
         bool external_logging;
     };
     class HttpConnection;
@@ -44,6 +66,7 @@ namespace http
     public:
         /// @brief Opens an HTTP/1.1 on the specified port. Over TCP.
         /// @param config Configuration for the HTTP server.
+        /// @param handler The request handler function that will be called for each incoming HTTP request. It should be a function that takes a const reference to an HttpRequest and a non-const reference to an HttpResponse, and returns void. This function will be responsible for processing the incoming requests and generating appropriate responses.
         /// @throws http::exceptions::CanNotCreateServer if the server cannot be created.
         explicit HttpServer(HttpServerConfig config, RequestHandler handler);
 
