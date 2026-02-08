e03da8be23b248c33261c2451c5b2617ae9c0b0a
ansh1406 <anshswaroop14@gmail.com>
2026-02-08 17:14:43 +0530

    Documentation

--- include/http/http_response.hpp
+++ include/http/http_response.hpp
@@ -1,3 +1,6 @@
+/// @file http_response.hpp
+/// @brief This file defines the HttpResponse class, which represents an HTTP response.
+
 #ifndef HTTP_RESPONSE_HPP
 #define HTTP_RESPONSE_HPP
 
@@ -9,25 +12,38 @@
 
 namespace http
 {
-    /// @brief Class representing an HTTP response.
     class HttpResponse
     {
     private:
+        /// @brief  The HTTP version (e.g., HTTP/1.1). It is set to HTTP/1.1 by default and cannot be changed because of library constraints.
         std::string _version;
+        /// @brief The HTTP status code (e.g., 200, 404).
         int _status_code;
+        /// @brief The HTTP resaon phrase (e.g., "OK", "Not Found").
         std::string _status_message;
+        /// @brief A map of HTTP headers. The keys are header names (case-insensitive), and the values are header values.
         std::map<std::string, std::string> _headers;
+        /// @brief The body of the HTTP response, stored as a vector of bytes. It will be empty for responses that do not have a body.
         std::vector<char> _body;
 
     public:
+        /// @brief Constructor for HttpResponse with all parameters.
+        /// @param status_code The HTTP status code (e.g., 200, 404).
+        /// @param status_message The HTTP status message (e.g., "OK", "Not Found").
+        /// @param headers A map of HTTP headers. The keys are header names (case-insensitive), and the values are header values.
+        /// @param body The body of the HTTP response, stored as a vector of bytes. It will be empty for responses that do not have a body.
         HttpResponse(int status_code,
                      const std::string &status_message,
                      const std::map<std::string, std::string> &headers,
                      const std::vector<char> &body)
             : _version(versions::HTTP_1_1), _status_code(status_code), _status_message(status_message), _headers(headers), _body(body) {}
 
+        /// @brief Default constructor for HttpResponse. Initializes an empty HTTP response with HTTP version set to HTTP/1.1. Verison is set to HTTP/1.1 by default and cannot be changed because of library constraints.
         HttpResponse() : _version(versions::HTTP_1_1) {}
 
+        /// @brief Constructor for HttpResponse with status code and message.
+        /// @param status_code The HTTP status code (e.g., 200, 404).
+        /// @param status_message The HTTP status message (e.g., "OK", "Not Found").
         explicit HttpResponse(int status_code,
                               const std::string &status_message)
             : _version(versions::HTTP_1_1), _status_code(status_code), _status_message(status_message) {}
