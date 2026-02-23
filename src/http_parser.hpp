a7a6b2f7441c13fcbe0323bc3748359608565d39
ansh1406 <anshswaroop14@gmail.com>
2026-02-23 23:19:51 +0530

    Docs update

--- src/http_parser.hpp
+++ src/http_parser.hpp
@@ -41,12 +41,25 @@ namespace http
         static std::vector<char> parse_body(const std::vector<char> &raw_request, size_t &pos, const std::map<std::string, std::string> &headers);
 
         /// @brief Validates the request line format.
+        /// @param request_line_byte_buffer vector of chars representing the request line.
+        /// @return Returns true if the request line is valid, false otherwise.
         static bool validate_request_line(const std::vector<char> &request_line_byte_buffer);
 
+        /// @brief Checks if the header is a Content-Length header and returns its value if it is.
+        /// @param header_byte_buffer vector of chars representing the header line.
+        /// @return Returns the value of the Content-Length header if it exists, -1 otherwise.
+        /// @throws http::exceptions::InvalidContentLength if the header is Content-Length but Content-Length value is invalid (e.g., negative or non-numeric).
         static long is_content_length_header(const std::vector<char> &header_byte_buffer);
 
+        /// @brief Checks if the header is a Transfer-Encoding header with chunked value.
+        /// @param header_byte_buffer vector of chars representing the header line.
+        /// @return Returns true if the Transfer-Encoding header is chunked, false otherwise.
+        /// @throws http::exceptions::TransferEncodingWithoutChunked if the header is Transfer-Encoding but value is not chunked.
         static bool is_transfer_encoding_chunked_header(const std::vector<char> &header_byte_buffer);
 
+        /// @brief Creates a response buffer from an HttpResponse object.
+        /// @param response The HttpResponse object to be converted into a response buffer.
+        /// @return Returns a vector of chars representing the raw HTTP response that can be sent to the client.
         static std::vector<char> create_response_buffer(const http::HttpResponse &response);
     };
 }
