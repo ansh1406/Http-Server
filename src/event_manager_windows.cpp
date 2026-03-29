2ab5059efa833355c086b3937de2725298d246ae
ansh1406 <anshswaroop14@gmail.com>
2026-03-30 00:20:04 +0530

    Added implementation of event manager for windows

---
+++ src/event_manager_windows.cpp
@@ -0,0 +1,210 @@
+#ifdef _WIN32
+
+#include "event_manager.hpp"
+#include "wepoll/wepoll.h"
+
+#include <winsock2.h>
+
+namespace
+{
+	HANDLE epoll_handle = nullptr;
+
+	std::string wsa_error_message(const std::string &prefix)
+	{
+		return prefix + std::to_string(WSAGetLastError());
+	}
+}
+
+namespace tcp
+{
+	EventManager::EventManager(const int max_events, const time_t timeout) : epoll_fd(-1), max_events(max_events), timeout(timeout)
+	{
+		HANDLE handle = epoll_create1(0);
+		if (handle == nullptr)
+		{
+			throw exceptions::CanNotCreateEventManager(wsa_error_message("Failed to create epoll instance: "));
+		}
+		epoll_handle = handle;
+
+		epoll_fd = 1;
+	}
+
+	EventManager::EventManager(EventManager &&other) noexcept
+		: epoll_fd(other.epoll_fd), status(std::move(other.status)), max_events(other.max_events), timeout(other.timeout)
+	{
+		other.epoll_fd = -1;
+	}
+
+	EventManager &EventManager::operator=(EventManager &&other) noexcept
+	{
+		if (this != &other)
+		{
+			if (epoll_fd != -1)
+			{
+				epoll_close(epoll_handle);
+				epoll_handle = nullptr;
+			}
+
+			epoll_fd = other.epoll_fd;
+			status = std::move(other.status);
+			max_events = other.max_events;
+			timeout = other.timeout;
+			other.epoll_fd = -1;
+		}
+		return *this;
+	}
+
+	int EventManager::register_socket(const int fd)
+	{
+		try
+		{
+			epoll_event ev{};
+			ev.events = EPOLLIN;
+			ev.data.fd = fd;
+
+			if (epoll_ctl(epoll_handle, EPOLL_CTL_ADD, static_cast<SOCKET>(fd), &ev) != 0)
+			{
+				throw exceptions::CanNotRegisterSocket(wsa_error_message("Failed to register socket: "));
+			}
+
+			status[fd] = socket_status::WRITABLE;
+			return fd;
+		}
+		catch (exceptions::CanNotRegisterSocket &)
+		{
+			throw;
+		}
+		catch (std::exception &e)
+		{
+			throw exceptions::CanNotRegisterSocket("Failed to register socket: " + std::string(e.what()));
+		}
+		catch (...)
+		{
+			throw exceptions::CanNotRegisterSocket("Failed to register socket: Unknown error");
+		}
+	}
+
+	void EventManager::add_to_write_monitoring(const int id)
+	{
+		try
+		{
+			epoll_event ev{};
+			ev.events = EPOLLIN | EPOLLOUT;
+			ev.data.fd = id;
+
+			if (epoll_ctl(epoll_handle, EPOLL_CTL_MOD, static_cast<SOCKET>(id), &ev) != 0)
+			{
+				throw exceptions::CanNotModifySocket(wsa_error_message("Failed to modify socket for write monitoring: "));
+			}
+		}
+		catch (exceptions::CanNotModifySocket &)
+		{
+			throw;
+		}
+		catch (std::exception &e)
+		{
+			throw exceptions::CanNotModifySocket("Failed to modify socket for write monitoring: " + std::string(e.what()));
+		}
+		catch (...)
+		{
+			throw exceptions::CanNotModifySocket("Failed to modify socket for write monitoring: Unknown error");
+		}
+	}
+
+	void EventManager::remove_socket(const int id)
+	{
+		try
+		{
+			if (epoll_ctl(epoll_handle, EPOLL_CTL_DEL, static_cast<SOCKET>(id), nullptr) != 0)
+			{
+				throw exceptions::CanNotRemoveSocket(wsa_error_message("Failed to remove socket: "));
+			}
+
+			status.erase(id);
+		}
+		catch (exceptions::CanNotRemoveSocket &)
+		{
+			throw;
+		}
+		catch (std::exception &e)
+		{
+			throw exceptions::CanNotRemoveSocket("Failed to remove socket: " + std::string(e.what()));
+		}
+		catch (...)
+		{
+			throw exceptions::CanNotRemoveSocket("Failed to remove socket: Unknown error");
+		}
+	}
+
+	EventManager::~EventManager()
+	{
+		if (epoll_fd == -1)
+		{
+			return;
+		}
+
+		epoll_close(epoll_handle);
+		epoll_handle = nullptr;
+	}
+
+	std::vector<int> EventManager::wait_for_events()
+	{
+		try
+		{
+			std::vector<epoll_event> events(max_events);
+			int num_events = epoll_wait(epoll_handle, events.data(), max_events, static_cast<int>(timeout));
+			if (num_events == -1)
+			{
+				throw exceptions::CanNotWaitForEvents(wsa_error_message("Failed to wait for events: "));
+			}
+
+			std::vector<int> fds;
+			fds.reserve(static_cast<size_t>(num_events));
+
+			for (int i = 0; i < num_events; ++i)
+			{
+				int fd = events[i].data.fd;
+				if (events[i].events & EPOLLIN)
+				{
+					status[fd] |= socket_status::READABLE;
+				}
+				if (events[i].events & EPOLLOUT)
+				{
+					status[fd] |= socket_status::WRITABLE;
+				}
+				fds.push_back(fd);
+			}
+
+			return fds;
+		}
+		catch (exceptions::CanNotWaitForEvents &)
+		{
+			throw;
+		}
+		catch (std::exception &e)
+		{
+			throw exceptions::CanNotWaitForEvents("Failed to wait for events: " + std::string(e.what()));
+		}
+		catch (...)
+		{
+			throw exceptions::CanNotWaitForEvents("Failed to wait for events: Unknown error");
+		}
+	}
+
+	const bool EventManager::is_readable(const int id) const
+	{
+		return status.at(id) & socket_status::READABLE;
+	}
+
+	const bool EventManager::is_writable(const int id) const
+	{
+		return status.at(id) & socket_status::WRITABLE;
+	}
+
+	void EventManager::clear_status(const int id)
+	{
+		status[id] = socket_status::IDLE;
+	}
+}
+
+#endif
