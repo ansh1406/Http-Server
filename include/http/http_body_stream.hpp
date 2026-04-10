a3802ee482790d87bc53f8df08e5da51ee2bf212
ansh1406 <anshswaroop14@gmail.com>
2026-04-10 17:27:08 +0000

    Body stream classes prototype

---
+++ include/http/http_body_stream.hpp
@@ -0,0 +1,37 @@
+#ifndef HTTP_BODY_STREAM_HPP
+#define HTTP_BODY_STREAM_HPP
+
+#include <vector>
+#include <functional>
+
+namespace http
+{
+    class RequestBodyStream
+    {
+    private:
+        struct Impl;
+        Impl *pimpl;
+
+    public:
+        RequestBodyStream();
+        ~RequestBodyStream();
+
+        bool has_more_data() const;
+        bool is_stream_closed() const;
+        size_t get_next(std::vector<char> &buffer, size_t buffer_cursor = 0);
+    };
+
+    class ResponseBodyStream
+    {
+    private:
+        struct Impl;
+        Impl *pimpl;
+
+    public:
+        using WriterFunction = std::function<void(std::vector<char> &data)>;
+        ResponseBodyStream(WriterFunction writer);
+        ~ResponseBodyStream();
+    };
+}
+
+#endif // HTTP_BODY_STREAM_HPP
