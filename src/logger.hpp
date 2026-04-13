598b37e84a5f8e3c109d046821c72d0af456a781
ansh1406 <anshswaroop14@gmail.com>
2026-04-13 19:15:42 +0000

    Updated logger for multithread support

--- src/logger.hpp
+++ src/logger.hpp
@@ -4,6 +4,22 @@
 #include <ctime>
 #include <stdexcept>
 
+#include <mutex>
+
+namespace
+{
+    std::tm get_local_time(std::time_t t)
+    {
+        std::tm tm{};
+#ifdef _WIN32
+        localtime_s(&tm, &t);
+#else
+        localtime_r(&t, &tm);
+#endif
+        return tm;
+    }
+}
+
 class Logger
 {
 public:
@@ -20,13 +36,7 @@ public:
         explicit CanNotInitializeLogger() : std::runtime_error("Logger initialization failed") {};
     };
 
-    ~Logger()
-    {
-        if (external_log && log_file.is_open())
-        {
-            log_file.close();
-        }
-    }
+    ~Logger() = default;
 
     static Logger &get_instance()
     {
@@ -78,15 +88,19 @@ public:
 
             std::time_t now = std::time(nullptr);
             char time_buf[20];
-            std::strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+            std::strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", &::get_local_time(now));
 
-            if (external_log)
             {
-                log_file << "[" << time_buf << "] [" << level_str << "] " << message << std::endl;
-            }
-            else
-            {
-                std::cout << "[" << time_buf << "] [" << level_str << "] " << message << std::endl;
+                std::lock_guard<std::mutex> lock(log_mutex);
+                std::string log_line = "[" + std::string(time_buf) + "] [" + level_str + "] " + message;
+                if (external_log)
+                {
+                    log_file << log_line << std::endl;
+                }
+                else
+                {
+                    std::cout << log_line << std::endl;
+                }
             }
         }
         catch (...)
@@ -95,8 +109,19 @@ public:
         }
     }
 
+    static void shutdown()
+    {
+        Logger &logger = get_instance();
+        if (logger.external_log && logger.log_file.is_open())
+        {
+            logger.log_file.close();
+        }
+    }
+
 private:
     std::ofstream log_file;
     bool external_log = false;
+    std::mutex log_mutex;
+
     Logger() = default;
 };
