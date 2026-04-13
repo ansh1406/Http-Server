c83e823423e9d081795ef5a2d3377f93aa6aef5f
ansh1406 <anshswaroop14@gmail.com>
2026-04-13 21:24:38 +0000

    Request body stream invalid state fix

--- src/http_request.cpp
+++ src/http_request.cpp
@@ -10,40 +10,22 @@ namespace http
         DataStream data_stream;
     };
 
-    HttpRequest::RequestBodyStream::RequestBodyStream()
-    {
-        pimpl = nullptr;
-    }
+    HttpRequest::RequestBodyStream::RequestBodyStream() : pimpl(new Impl()) {}
 
-    HttpRequest::RequestBodyStream::~RequestBodyStream()
-    {
-        delete pimpl;
-    }
+    HttpRequest::RequestBodyStream::~RequestBodyStream() { delete pimpl; }
 
     size_t HttpRequest::RequestBodyStream::get_next(std::vector<char> &buffer, size_t buffer_cursor)
     {
-        if (!pimpl)
-        {
-            throw std::runtime_error("RequestBodyStream: Attempted to read from an uninitialized stream.");
-        }
         return pimpl->data_stream.get_next(buffer, buffer_cursor);
     }
 
     bool HttpRequest::RequestBodyStream::has_more_data() const
     {
-        if (!pimpl)
-        {
-            throw std::runtime_error("RequestBodyStream: Attempted to check for more data on an uninitialized stream.");
-        }
         return pimpl->data_stream.has_more_data();
     }
 
     bool HttpRequest::RequestBodyStream::is_stream_closed() const
     {
-        if (!pimpl)
-        {
-            throw std::runtime_error("RequestBodyStream: Attempted to check if stream is closed on an uninitialized stream.");
-        }
         return pimpl->data_stream.is_stream_closed();
     }
 
@@ -83,7 +65,6 @@ namespace http
     HttpRequest::RequestBodyStream HttpRequestBuilder::build_body_stream(DataStream &&data_stream)
     {
         HttpRequest::RequestBodyStream stream;
-        stream.pimpl = new HttpRequest::RequestBodyStream::Impl();
         stream.pimpl->data_stream = std::move(data_stream);
         return stream;
     }
