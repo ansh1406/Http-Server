a3c29d206cf3f7458640fdb8c14aa2a2d50a3bb0
ansh1406 <anshswaroop14@gmail.com>
2026-04-13 21:51:08 +0000

    Updated parser

--- src/http_parser.cpp
+++ src/http_parser.cpp
@@ -74,46 +74,17 @@ std::map<std::string, std::string> http::HttpParser::parse_headers(const std::ve
         std::string value = std::string(raw_request.begin() + start, raw_request.begin() + pos);
         pos += 2;
 
-        headers[key] = value;
+        if (headers.find(key) != headers.end())
+        {
+            /// TODO: Handle duplicate header
+        }
+        else
+        {
+            headers[key] = value;
+        }
     }
     return headers;
 }
-
-std::vector<char> http::HttpParser::parse_body(const std::vector<char> &raw_request, size_t &pos, const std::map<std::string, std::string> &headers)
-{
-    std::vector<char> body;
-    auto it = headers.find(http::headers::CONTENT_LENGTH);
-    if (it != headers.end())
-    {
-        size_t content_length = std::stoul(it->second);
-        body.insert(body.end(), raw_request.begin() + pos, raw_request.begin() + pos + content_length);
-        pos += content_length;
-    }
-    else if (headers.find(http::headers::TRANSFER_ENCODING) != headers.end())
-    {
-        while (true)
-        {
-            size_t start = pos;
-            while (pos < raw_request.size() && !(raw_request[pos] == '\r' && raw_request[pos + 1] == '\n'))
-            {
-                pos++;
-            }
-            std::string chunk_size_str(raw_request.begin() + start, raw_request.begin() + pos);
-            size_t chunk_size = std::stoul(chunk_size_str, nullptr, 16);
-            pos += 2;
-
-            if (chunk_size == 0)
-            {
-                break;
-            }
-
-            body.insert(body.end(), raw_request.begin() + pos, raw_request.begin() + pos + chunk_size);
-            pos += chunk_size + 2; // Skip CRLF
-        }
-    }
-    return body;
-}
-
 
 bool http::HttpParser::validate_request_line(const std::vector<char> &request_line_byte_buffer)
 {
@@ -134,61 +105,29 @@ bool http::HttpParser::validate_request_line(const std::vector<char> &request_li
     return space_count == 2;
 }
 
-bool http::HttpParser::is_transfer_encoding_chunked_header(const std::vector<char> &header_byte_buffer)
+bool http::HttpParser::has_transfer_encoding_chunked_header(const std::map<std::string, std::string> &headers)
 {
-    std::string header_line(header_byte_buffer.begin(), header_byte_buffer.end());
-    size_t colon_pos = header_line.find(':');
-    if (colon_pos == std::string::npos)
+    auto it = headers.find(http::headers::TRANSFER_ENCODING);
+    if (it != headers.end())
     {
-        return false;
-    }
-
-    std::string key = header_line.substr(0, colon_pos);
-    for (auto &c : key)
-        c = std::tolower(c);
-    std::string value = header_line.substr(colon_pos + 1);
-
-    while (!value.empty() && (value.front() == ' ' || value.front() == '\t'))
-        value.erase(value.begin());
-    if (key == http::headers::TRANSFER_ENCODING)
-    {
-        size_t last_comma = value.find_last_of(',');
-        std::string last_encoding = (last_comma == std::string::npos) ? value : value.substr(last_comma + 1);
-
-        while (!last_encoding.empty() && (last_encoding.front() == ' ' || last_encoding.front() == '\t'))
-            last_encoding.erase(last_encoding.begin());
-
-        while (!last_encoding.empty() && (last_encoding.back() == ' ' || last_encoding.back() == '\t'))
-            last_encoding.pop_back();
-
-        if (last_encoding != "chunked")
+        const std::string &value = it->second;
+        size_t chunked_pos = value.find("chunked");
+        if (chunked_pos != std::string::npos)
         {
-            throw http::exceptions::TransferEncodingWithoutChunked();
+            return true;
         }
-        return true;
     }
     return false;
 }
 
-long http::HttpParser::is_content_length_header(const std::vector<char> &header_byte_buffer)
+long http::HttpParser::has_content_length_header(const std::map<std::string, std::string> &headers)
 {
-    std::string header_line(header_byte_buffer.begin(), header_byte_buffer.end());
-    size_t colon_pos = header_line.find(':');
-    if (colon_pos == std::string::npos)
-    {
-        return -1;
-    }
-    std::string key = header_line.substr(0, colon_pos);
-    for (auto &c : key)
-        c = std::tolower(c);
-    std::string value = header_line.substr(colon_pos + 1);
-    while (!value.empty() && (value.front() == ' ' || value.front() == '\t'))
-        value.erase(value.begin());
-    if (key == http::headers::CONTENT_LENGTH)
+    auto it = headers.find(http::headers::CONTENT_LENGTH);
+    if (it != headers.end())
     {
         try
         {
-            size_t content_length = std::stol(value);
+            size_t content_length = std::stol(it->second);
             if (content_length < 0)
             {
                 throw http::exceptions::InvalidContentLength();
@@ -201,24 +140,4 @@ long http::HttpParser::is_content_length_header(const std::vector<char> &header_
         }
     }
     return -1;
-}
-
-std::vector<char> http::HttpParser::create_response_buffer(const http::HttpResponse &response)
-{
-    std::vector<char> buffer;
-    std::string status_line = response.version() + " " + std::to_string(response.status_code()) + " " + response.status_message() + "\r\n";
-    buffer.insert(buffer.end(), status_line.begin(), status_line.end());
-
-    for (const auto &header : response.headers())
-    {
-        std::string header_line = header.first + ": " + header.second + "\r\n";
-        buffer.insert(buffer.end(), header_line.begin(), header_line.end());
-    }
-
-    buffer.insert(buffer.end(), '\r');
-    buffer.insert(buffer.end(), '\n');
-
-    buffer.insert(buffer.end(), response.body().begin(), response.body().end());
-
-    return buffer;
 }
--- src/http_parser.hpp
+++ src/http_parser.hpp
@@ -32,13 +32,6 @@ namespace http
         /// @return Returns a map of header key-value pairs. All keys are converted to lowercase.
         static std::map<std::string, std::string> parse_headers(const std::vector<char> &raw_request, size_t &pos);
 
-        /// @brief Parses the body from the raw HTTP request
-        /// @param raw_request vector of chars representing the raw HTTP request.
-        /// @param pos Starting position for parsing, will be updated to the position after the body
-        /// @param headers Map of header key-value pairs to determine body length.
-        /// @return Returns a vector of chars representing the body.
-        static std::vector<char> parse_body(const std::vector<char> &raw_request, size_t &pos, const std::map<std::string, std::string> &headers);
-
         /// @brief Validates the request line format.
         /// @param request_line_byte_buffer vector of chars representing the request line.
         /// @return Returns true if the request line is valid, false otherwise.
@@ -48,18 +41,13 @@ namespace http
         /// @param header_byte_buffer vector of chars representing the header line.
         /// @return Returns the value of the Content-Length header if it exists, -1 otherwise.
         /// @throws http::exceptions::InvalidContentLength if the header is Content-Length but Content-Length value is invalid (e.g., negative or non-numeric).
-        static long is_content_length_header(const std::vector<char> &header_byte_buffer);
+        static long has_content_length_header(const std::map<std::string, std::string> &headers);
 
         /// @brief Checks if the header is a Transfer-Encoding header with chunked value.
         /// @param header_byte_buffer vector of chars representing the header line.
         /// @return Returns true if the Transfer-Encoding header is chunked, false otherwise.
         /// @throws http::exceptions::TransferEncodingWithoutChunked if the header is Transfer-Encoding but value is not chunked.
-        static bool is_transfer_encoding_chunked_header(const std::vector<char> &header_byte_buffer);
-
-        /// @brief Creates a response buffer from an HttpResponse object.
-        /// @param response The HttpResponse object to be converted into a response buffer.
-        /// @return Returns a vector of chars representing the raw HTTP response that can be sent to the client.
-        static std::vector<char> create_response_buffer(const http::HttpResponse &response);
+        static bool has_transfer_encoding_chunked_header(const std::map<std::string, std::string> &headers);
     };
 }
 
