fe06dd1c640ee288d9d3c775b5307cb56ffb7568
ansh1406 <anshswaroop14@gmail.com>
2026-04-16 03:11:32 +0530

    fixed receive data function for windows

--- src/tcp_windows.cpp
+++ src/tcp_windows.cpp
@@ -206,49 +206,52 @@ namespace tcp
         }
     }
 
-    std::vector<char> ConnectionSocket::receive_data()
+    size_t ConnectionSocket::receive_data(std::vector<char> &buffer, size_t buffer_cursor, bool read_once)
     {
         try
         {
-            std::vector<char> buffer;
             size_t total_received = 0;
             while (true)
             {
-                int remaining_space = static_cast<int>(buffer.size() - total_received);
-                if (remaining_space < static_cast<int>(constants::BUFFER_EXPANTION_SIZE))
+                int remaining_space = (buffer.size() - buffer_cursor) - total_received;
+                if (remaining_space <= 0)
                 {
-                    buffer.resize(buffer.size() + constants::BUFFER_EXPANTION_SIZE);
-                    remaining_space = static_cast<int>(buffer.size() - total_received);
+                    break;
                 }
-                int bytes_received = recv(socket_fd.fd(), buffer.data() + total_received, remaining_space, 0);
+
+                long bytes_received = recv(socket_fd.fd(), buffer.data() + buffer_cursor + total_received, remaining_space, 0);
+
                 if (bytes_received == 0)
                 {
-                    throw exceptions::CanNotReceiveData{"Connection closed by peer."};
+                    throw tcp::exceptions::CanNotReceiveData{"Connection closed by peer."};
                 }
-                if (bytes_received == SOCKET_ERROR)
+                if (bytes_received < 0)
                 {
-                    int err = WSAGetLastError();
-                    if (err == WSAEWOULDBLOCK)
+                    int err = errno;
+                    if (err == EAGAIN || err == EWOULDBLOCK)
                     {
                         break;
                     }
                     else
                     {
-                        throw exceptions::CanNotReceiveData{get_error_message()};
+                        throw tcp::exceptions::CanNotReceiveData{std::string(strerror(err))};
                     }
                 }
                 total_received += bytes_received;
+                if (read_once)
+                {
+                    break;
+                }
             }
-            buffer.resize(total_received);
-            return buffer;
+            return total_received;
         }
         catch (const std::exception &e)
         {
-            throw exceptions::CanNotReceiveData{"TCP: Failed to receive data: " + std::string(e.what())};
+            throw tcp::exceptions::CanNotReceiveData{"TCP: Failed to receive data: " + std::string(e.what())};
         }
         catch (...)
         {
-            throw exceptions::CanNotReceiveData{"TCP: Unknown error while receiving data."};
+            throw tcp::exceptions::CanNotReceiveData{"TCP: Unknown error while receiving data."};
         }
     }
 
