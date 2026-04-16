bae15dbf40132896310e796ffb06528791444fe8
ansh1406 <anshswaroop14@gmail.com>
2026-04-16 18:09:39 +0530

    Added docs for http request

--- include/http/http_request.hpp
+++ include/http/http_request.hpp
@@ -10,9 +10,11 @@
 #include <stdexcept>
 namespace http
 {
+    /// @brief Container for HTTP request data.
     class HttpRequest
     {
     public:
+        /// @brief A stream-like interface for reading the body of an HTTP request.
         struct RequestBodyStream
         {
         private:
@@ -34,8 +36,20 @@ namespace http
             RequestBodyStream(RequestBodyStream &&other) noexcept;
             RequestBodyStream &operator=(RequestBodyStream &&other) noexcept;
 
+            /// @brief Checks if there is more data available in the stream.
+            /// @return True if there is more data, false otherwise.
             bool has_more_data() const;
+
+            /// @brief Checks if the stream is closed.
+            /// @return True if the stream is closed, false otherwise.
             bool is_stream_closed() const;
+
+            /// @brief Reads the next chunk of data from the stream.
+            /// @param buffer The buffer to read data into.
+            /// @param buffer_cursor The position in the buffer to start reading from.
+            /// @param max_size The maximum number of bytes to read.
+            /// @return The number of bytes read.
+            /// @throws StreamError if an error occurs while reading.
             size_t get_next(std::vector<char> &buffer, size_t buffer_cursor = 0, size_t max_size = static_cast<size_t>(-1));
 
             friend struct HttpRequestBuilder;
@@ -69,16 +83,22 @@ namespace http
 
         /// @return The IP address of the client making the request as a std::string.
         const std::string &ip() const noexcept;
+
         /// @return The port number from which the client is connecting as a std::string.
         const std::string &port() const noexcept;
+
         /// @return The HTTP method (e.g., GET, POST) as a std::string.
         const std::string &method() const noexcept;
+
         /// @return The requested URI (e.g., /index.html) as a std::string. It is stored as a URL encoded value.
         const std::string &uri() const noexcept;
+
         /// @return The HTTP version (e.g., HTTP/1.1) as a std::string.
         const std::string &version() const noexcept;
+
         /// @return The HTTP headers as a map of header key(std::string)-value(std::string) pairs.
         const std::map<std::string, std::string> &headers() const noexcept;
+
         /// @return The body of the HTTP request.
         const RequestBodyStream &body() const noexcept;
 
